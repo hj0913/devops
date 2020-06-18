26fe3365aae0ccaa8d3deeec5395622046131a9c
hj0913 <18168875593@163.com>
2020-06-18 11:42:13 +0800

    文件工程目录ä¼创建

---
+++ devops_java/devops_java_thread/src/main/java/com/devops/devops_java_thread/DevopsJavaThreadApplication.java
@@ -0,0 +1,13 @@
+package com.devops.devops_java_thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsJavaThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsJavaThreadApplication.class, args);
+    }
+
+}
